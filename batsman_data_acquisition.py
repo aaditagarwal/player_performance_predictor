dc9da1063be5d5748c412ff1bcd3d15ef38f21dc
aaditagarwal <agarwal.aadit99@gmail.com>
2020-07-21 22:38:13 +0530

    player_details

---
+++ batsman_data_acquisition.py
@@ -0,0 +1,149 @@
+import pandas as pd
+import yaml
+import os
+import time
+import numpy as np
+import warnings
+
+warnings.filterwarnings("ignore")  # Hide messy Numpy warnings
+
+def extract_details(filename):
+    dict = yaml.load(open(filename))
+    flag = False
+    
+    match_details = {}
+    match_details['venue'] = dict['info']['venue']
+    match_details['date'] = dict['info']['dates'][0]
+    match_details['team1'] = dict['info']['teams'][0]
+    match_details['team2'] = dict['info']['teams'][1]
+    if 'winner' not in dict['info']['outcome']:
+        match_details['winner'] = 'no_result'
+    else:
+        match_details['winner'] = dict['info']['outcome']['winner']
+    if dict['info']['toss']['winner'] == match_details['team1']:
+        if dict['info']['toss']['decision'] == 'bat':
+            match_details['bat_first'] = ([match_details['team1'],2])
+        else:
+            match_details['bat_first'] = ([match_details['team2'],1])
+    else:
+        if dict['info']['toss']['decision'] == 'bat':
+            match_details['bat_first'] = ([match_details['team2'],1])
+        else:
+            match_details['bat_first'] = ([match_details['team1'],2])
+    print(match_details)
+
+    player_details = pd.DataFrame(columns=['date','team','runs','balls','not_out','opposition','batting_order','venue','outcome'])
+    
+    innings1 = dict['innings'][0]['1st innings']['deliveries']
+    for ball in innings1:
+        for delivery in ball:
+      
+            try:
+                player_details.index.get_loc(ball[delivery]['batsman'])
+            except:
+                player_details.loc[ball[delivery]['batsman'],'runs'] = 0
+                player_details.loc[ball[delivery]['batsman'], 'balls'] = 0
+            
+            player_details.loc[ball[delivery]['batsman'], 'date'] = match_details['date']
+            player_details.loc[ball[delivery]['batsman'], 'team'] = match_details['bat_first'][0]
+            player_details.loc[ball[delivery]['batsman'], 'oppostion'] = match_details['team'+str(match_details['bat_first'][1])]
+            player_details.loc[ball[delivery]['batsman'], 'batting_order'] = 1
+            player_details.loc[ball[delivery]['batsman'], 'venue'] = match_details['venue']
+            if match_details['winner'] == player_details.loc[ball[delivery]['batsman'], 'team']:
+                player_details.loc[ball[delivery]['batsman'], 'outcome'] = 1
+            else:
+                player_details.loc[ball[delivery]['batsman'], 'outcome'] = 0
+            if 'wicket' in ball[delivery]:
+                player_details.loc[ball[delivery]['batsman'],'not_out'] = False
+            else:
+                player_details.loc[ball[delivery]['batsman'], 'not_out'] = True
+            
+            player_details.loc[ball[delivery]['batsman'],'runs'] += ball[delivery]['runs']['batsman']
+            player_details.loc[ball[delivery]['batsman'],'balls'] += 1
+            
+    try:
+        innings2 = dict['innings'][1]['2nd innings']['deliveries']
+    except:
+        flag = True
+    else:
+        for ball in innings2:
+            for delivery in ball:
+            
+                try:
+                    player_details.index.get_loc(ball[delivery]['batsman'])
+                except:
+                    player_details.loc[ball[delivery]['batsman'],'runs'] = 0
+                    player_details.loc[ball[delivery]['batsman'], 'balls'] = 0
+
+                player_details.loc[ball[delivery]['batsman'], 'team'] = match_details['team'+str(match_details['bat_first'][1])]
+                player_details.loc[ball[delivery]['batsman'], 'opposition'] = match_details['bat_first']
+                player_details.loc[ball[delivery]['batsman'], 'batting_order'] = 2
+                player_details.loc[ball[delivery]['batsman'], 'venue'] = match_details['venue']
+                if match_details['winner'] == player_details.loc[ball[delivery]['batsman'], 'team']:
+                    player_details.loc[ball[delivery]['batsman'], 'outcome'] = 1
+                else:
+                    player_details.loc[ball[delivery]['batsman'], 'outcome'] = 0
+                if 'wicket' in ball[delivery]:
+                    player_details.loc[ball[delivery]['batsman'],'not_out'] = False
+                else:
+                    player_details.loc[ball[delivery]['batsman'], 'not_out'] = True
+
+                player_details.loc[ball[delivery]['batsman'],'runs'] += ball[delivery]['runs']['batsman']
+                player_details.loc[ball[delivery]['batsman'],'balls'] += 1                
+
+    return player_details
+
+overall_batsman_details = pd.DataFrame(columns=['team','innings', 'average', 'strike_rate','fifties','centuries','zeros','runs','balls'])
+match_batsman_details = pd.DataFrame(columns=['date','name','team','opposition','venue','batting_order','outcome','runs','balls','not_out','strike_rate'])
+
+count = -1
+
+for filename in os.listdir('odis'):
+    if filename.endswith('.yaml'):
+
+        player_details = extract_details('./odis/'+filename)
+
+        for player in player_details.index:
+            count += 1
+            try:
+                overall_batsman_details.index.get_loc(player)
+            except:
+                overall_batsman_details.loc[player,'innings'] = 0
+                overall_batsman_details.loc[player,'centuries'] = 0
+                overall_batsman_details.loc[player,'fifties'] = 0
+                overall_batsman_details.loc[player,'zeros'] = 0
+                overall_batsman_details.loc[player,'strike_rate'] = 0
+                overall_batsman_details.loc[player,'runs'] = 0
+                overall_batsman_details.loc[player,'balls'] = 0
+            
+            overall_batsman_details.loc[player,'team'] = player_details.loc[player,'team']
+            overall_batsman_details.loc[player,'runs'] += player_details.loc[player,'runs']
+            overall_batsman_details.loc[player,'balls'] += player_details.loc[player,'balls']
+            overall_batsman_details.loc[player,'innings'] += 1
+            overall_batsman_details.loc[player,'strike_rate'] += overall_batsman_details.loc[player,'runs']/overall_batsman_details.loc[player,'balls']
+            if overall_batsman_details.loc[player,'runs'] == 0:
+                overall_batsman_details.loc[player,'zeros'] += 1
+            elif overall_batsman_details.loc[player,'runs']>=50 and overall_batsman_details.loc[player,'runs']<100:
+                overall_batsman_details.loc[player,'fifties'] += 1
+            elif overall_batsman_details.loc[player,'runs']>=100:
+                overall_batsman_details.loc[player,'centuries'] += 1
+            
+            match_batsman_details.loc[count,'date'] = player_details.loc[player,'date']
+            match_batsman_details.loc[count,'name'] = player
+            match_batsman_details.loc[count,'team'] = player_details.loc[player,'team']
+            match_batsman_details.loc[count,'opposition'] = player_details.loc[player,'opposition']
+            match_batsman_details.loc[count,'venue'] = player_details.loc[player,'venue']
+            match_batsman_details.loc[count,'batting_order'] = player_details.loc[player,'batting_order']
+            match_batsman_details.loc[count,'outcome'] = player_details.loc[player,'outcome']
+            match_batsman_details.loc[count,'runs'] = player_details.loc[player,'runs']
+            match_batsman_details.loc[count,'balls'] = player_details.loc[player,'balls']
+            match_batsman_details.loc[count,'not_out'] = player_details.loc[player,'not_out']
+            match_batsman_details.loc[count,'strike_rate'] = player_details.loc[player,'runs']/player_details.loc[player,'balls']
+
+overall_batsman_details['average'] = overall_batsman_details['runs']/overall_batsman_details['innings']
+overall_batsman_details['strike_rate'] /= overall_batsman_details['innings']
+
+match_batsman_details.set_index(keys=['date','name'],drop=True,inplace=True)
+
+overall_batsman_details.to_excel('./player_details/overall_batsman_details.xlsx')
+match_batsman_details.to_excel('./player_details/match_batsman_details.xlsx')
