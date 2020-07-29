4915d3498cfa7a48bd127d10dccba188ee3b4239
aaditagarwal <agarwal.aadit99@gmail.com>
2020-07-29 15:18:25 +0530

    error #2 update

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 #Importing Libraries
 import pandas as pd
 import numpy as np
-from scripts.player_performance.py import player_performance
-# import scripts.team_performance
+from scripts.player_performance import player_performance
+#from scripts.team_performance import team_performance
+
+import warnings
+from sklearn.exceptions import DataConversionWarning
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 #Input Function
 def input_values(input_value, check_list_1=None, check_list_2=None):
@@ -19,8 +23,8 @@ def input_values(input_value, check_list_1=None, check_list_2=None):
         if check_list_2 != None:
             if res in check_list_2:
                 flag += 2
-        if flag > 0:
-            return flag,res
+        if flag>0:
+        	return res,flag
         else:
             print('Invalid input.')
             continue
@@ -40,8 +44,9 @@ if __name__ == '__main__':
     match_bowler_details.loc[:, 'date'].ffill(inplace=True)
 
     #Input's
-    print('Available Services:\nSpecific Player Performance\n')
+    print('Available Services:\n1. Specific Player Performance\n')
     team,dump = input_values('team', match_batsman_details['team'].unique().tolist())
+    print(dump,team)
     player_name, param_player = input_values('player_name', match_batsman_details[match_batsman_details['team'] == team]['name'].unique().tolist(), match_bowler_details[match_bowler_details['team'] == team]['name'].unique().tolist())
     opposition, param_opp = input_values('opposition', match_batsman_details[match_batsman_details['name']==player_name]['opposition'].unique().tolist(), match_bowler_details[match_bowler_details['name'] == player_name]['opposition'].unique().tolist())
     venue, param_ven = input_values('venue', match_batsman_details[match_batsman_details['name']==player_name]['venue'].unique().tolist(), match_bowler_details[match_bowler_details['name']==player_name]['venue'].unique().tolist())
@@ -50,6 +55,9 @@ if __name__ == '__main__':
         param = param_opp
     if param > param_ven:
         param = param_ven
-    res = player_performance(param,player_name,team,opposition,venue)
-    print(res)
+    res = player_performance(param,player_name,opposition,venue)
+    if param == 1 or param == 3:
+        print(f'Number of predicted runs:{res["bat_prediction"]}')
+    elif param == 2 or param == 3:
+        print(f'Number of predicted wickets:{res["bowl_prediction"]}')
     exit()
--- scripts/player_performance.py
+++ scripts/player_performance.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 
 import warnings
+from sklearn.exceptions import DataConversionWarning
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # Initializing Models
@@ -24,41 +25,41 @@ def player_performance(param,player_name,opposition=None,venue=None):
 
     #Extracting Targets and Features
     if param == 1:
-    	overall_batsman_details = pd.read_excel('./player_details/overall_batsman_details.xlsx', header=0)
+        overall_batsman_details = pd.read_excel('./player_details/overall_batsman_details.xlsx', header=0)
         match_batsman_details = pd.read_excel('./player_details/match_batsman_details.xlsx',header=0)
         match_batsman_details.loc[:, 'date'].ffill(inplace=True)
 
         bat_match_details = match_batsman_details[match_batsman_details['name']==player_name]
         bat_overall_details = overall_batsman_details[overall_batsman_details['player_name']==player_name]
         bat_features = bat_match_details.loc[:,['opposition', 'venue', 'innings_played','previous_average', 'previous_strike_rate', 'previous_centuries','previous_fifties', 'previous_zeros']]
-        bat_targets = bat_match_details.loc[:,['runs']]
+        bat_targets = bat_match_details.loc[:,'runs']
             
     elif param == 2:
-    	overall_bowler_details = pd.read_excel('./player_details/overall_bowler_details.xlsx',header=0)
+        overall_bowler_details = pd.read_excel('./player_details/overall_bowler_details.xlsx',header=0)
         match_bowler_details = pd.read_excel('./player_details/match_bowler_details.xlsx',header=0)
         match_bowler_details.loc[:, 'date'].ffill(inplace=True)
 
         bowl_match_details = match_bowler_details[match_bowler_details['name']==player_name]
         bowl_overall_details = overall_bowler_details[overall_bowler_details['player_name']==player_name]
         bowl_features = bowl_match_details.loc[:,['opposition', 'venue', 'innings_played','previous_average', 'previous_strike_rate', 'previous_economy','previous_wicket_hauls']]
-        bowl_targets = bowl_match_details.loc[:,['wickets']]
+        bowl_targets = bowl_match_details.loc[:,'wickets']
 
     elif param == 3:
         overall_batsman_details = pd.read_excel('./player_details/overall_batsman_details.xlsx', header=0)
         match_batsman_details = pd.read_excel('./player_details/match_batsman_details.xlsx',header=0)
+        match_batsman_details.loc[:, 'date'].ffill(inplace=True)
+        bat_match_details = match_batsman_details[match_batsman_details['name']==player_name]
+        bat_overall_details = overall_batsman_details[overall_batsman_details['player_name']==player_name][['player_name','team','innings','runs','average','strike_rate','centuries','fifties','zeros']]
+        bat_features = bat_match_details.loc[:,['opposition', 'venue', 'innings_played','previous_average', 'previous_strike_rate', 'previous_centuries','previous_fifties', 'previous_zeros']]
+        bat_targets = bat_match_details.loc[:,['runs']]
+
         overall_bowler_details = pd.read_excel('./player_details/overall_bowler_details.xlsx',header=0)
         match_bowler_details = pd.read_excel('./player_details/match_bowler_details.xlsx',header=0)
-        match_batsman_details.loc[:, 'date'].ffill(inplace=True)
         match_bowler_details.loc[:, 'date'].ffill(inplace=True)
-
-        bat_match_details = match_batsman_details[match_batsman_details['name']==player_name]
         bowl_match_details = match_bowler_details[match_bowler_details['name'] == player_name]
-        bat_overall_details = overall_batsman_details[overall_batsman_details['player_name']==player_name][['player_name','team','innings','runs','average','strike_rate','centuries','fifties','zeros']]
-        bat_overall_details = overall_bowler_details[overall_bowler_details['player_name']==player_name][['player_name','team','innings','wickets','average','strike_rate','economy','wicket_hauls']]
-        bat_features = bat_match_details.loc[:,['opposition', 'venue', 'innings_played','previous_average', 'previous_strike_rate', 'previous_centuries','previous_fifties', 'previous_zeros']]
+        bowl_overall_details = overall_bowler_details[overall_bowler_details['player_name']==player_name][['player_name','team','innings','wickets','average','strike_rate','economy','wicket_hauls']]
         bowl_features = bowl_match_details.loc[:,['opposition', 'venue', 'innings_played','previous_average', 'previous_strike_rate', 'previous_economy','previous_wicket_hauls']]
-        bat_targets = bat_match_details.loc[['runs']]
-        bowl_targets = bowl_match_details.loc[['wickets']]
+        bowl_targets = bowl_match_details.loc[:,['wickets']]
 
     #Pre_Processing
     le = preprocessing.LabelEncoder()
@@ -70,15 +71,15 @@ def player_performance(param,player_name,opposition=None,venue=None):
         #Categorizing Runs
         bins = [0,30,60,100,250]
         labels = ["0","1","2","3"]
-        bat_targets = pd.cut(bat_targets,bins,labels=labels,include_lowest=True)
+        bat_targets = pd.cut(bat_targets['runs'],bins,labels=labels,include_lowest=True)
         
         #Categorizing Opposition and Venue
-        le.fit(bat_features.loc[:,['opposition']])
+        le.fit(bat_features.loc[:,'opposition'])
         opp_bat = le.transform([opposition])
-        le.fit(bat_features.loc[:,['venue']])
+        le.fit(bat_features.loc[:,'venue'])
         ven_bat = le.transform([venue])
-        bat_features.loc[:,['opposition']] = le.fit_transform(bat_features.loc[:,['opposition']])
-        bat_features.loc[:,['venue']] = le.fit_transform(bat_features.loc[:,['venue']])
+        bat_features.loc[:,'opposition'] = le.fit_transform(bat_features.loc[:,'opposition'])
+        bat_features.loc[:,'venue'] = le.fit_transform(bat_features.loc[:,'venue'])
 
         predict_bat = bat_overall_details[['innings','average','strike_rate','centuries','fifties','zeros']].values[0]
         
@@ -104,7 +105,7 @@ def player_performance(param,player_name,opposition=None,venue=None):
         best_params = None
             #XGBoost
         parameters = {'n_estimators':[75,100,125],'learning_rate':[0.1,0.01],'booster':['gbtree','dart']}
-        gridsearch_xgb = GridSearchCV(estimator=xgb_model,param_grid=parameters,scoring='accuracy',cv=2)
+        gridsearch_xgb = GridSearchCV(estimator=xgb,param_grid=parameters,scoring='accuracy',cv=2)
         gridresult_xgb = gridsearch_xgb.fit(bat_features,bat_targets)
         xgb_best_score = gridresult_xgb.best_score_
         xgb_best_params = gridresult_xgb.best_params_
@@ -112,7 +113,7 @@ def player_performance(param,player_name,opposition=None,venue=None):
         best_params = xgb_best_params
             #RandomForestClassifier
         parameters = {'n_estimators':[75,100,125],'criterion':['gini','entropy'],'min_samples_leaf':[1,2,3]}
-        gridsearch_rfc = GridSearchCV(estimator=rfc_model,param_grid=parameters,scoring='accuracy',cv=2)
+        gridsearch_rfc = GridSearchCV(estimator=rfc,param_grid=parameters,scoring='accuracy',cv=2)
         gridresult_rfc = gridsearch_rfc.fit(bat_features,bat_targets)
         rfc_best_score = gridresult_rfc.best_score_
         if best_score[0] < rfc_best_score:
@@ -120,7 +121,7 @@ def player_performance(param,player_name,opposition=None,venue=None):
             best_params = gridresult_rfc.best_params_
             #SupportVectorMachine
         parameters = {'C':[1,5,10],'kernel':['rbf','linear','sigmoid'],'gamma':['auto','scale']}
-        gridsearch_svc = GridSearchCV(estimator=svc_model,param_grid=parameters,scoring='accuracy',cv=5)
+        gridsearch_svc = GridSearchCV(estimator=svc,param_grid=parameters,scoring='accuracy',cv=5)
         gridresult_svc = gridsearch_svc.fit(bat_features,bat_targets)
         svc_best_score = gridresult_svc.best_score_
         if best_score[0] < svc_best_score:
@@ -136,17 +137,17 @@ def player_performance(param,player_name,opposition=None,venue=None):
         if best_score[1] == 'xgb':
             classifier = XGBclassifier(objective='multi:softmax',n_estimators=best_params['n_estimators'],learning_rate=best_params['learning_rate'],booster=best_params['booster'])
             classifier = classifier.fit(bat_features,bat_targets)
-            res = {'bat_prediction':classifier.predict(predict_bowl_features[0])}
+            res = {'bat_prediction':classifier.predict(predict_bat_features)}
             #RandomForestClassifier
         elif best_score[1] == 'rfc':
-            classifier = RandomForestClassifier(n_estimators=best_params['n_estiamtors'],criterion=best_params['criterion'],random_state=42,min_samples_leaf=best_params['min_samples_leaf'])
+            classifier = RandomForestClassifier(n_estimators=best_params['n_estimators'],criterion=best_params['criterion'],random_state=42,min_samples_leaf=best_params['min_samples_leaf'])
             classifier = classifier.fit(bat_features,bat_targets)
-            res = {'bat_prediction':classifier.predict(predict_bowl_features[0])}
+            res = {'bat_prediction':classifier.predict(predict_bat_features)}
             #SupportVectorMachine
         elif best_score[1] == 'svc':
             classifier = SVC(C=best_params['C'],kernel=best_params['kernel'],gamma=best_params['gamma'])
             classifier = classifier.fit(bat_features,bat_targets)
-            res = {'bat_prediction':classifier.predict(predict_bowl_features[0])}
+            res = {'bat_prediction':classifier.predict(predict_bat_features)}
  
         print('Batting Prediction Ends!')
 
@@ -189,24 +190,24 @@ def player_performance(param,player_name,opposition=None,venue=None):
         best_score = None
         best_params = None
             #XGBoost
-        parameters = [{'n_estimators':[75,100,125],'learning_rate':[0.1,0.01],'booster':['gbtree','dart']}]
-        gridsearch_xgb = GridSearchCV(estimator=xgb_model,param_grid=parameters,scoring='accuracy',cv=2)
+        parameters = {'n_estimators':[75,100,125],'learning_rate':[0.1,0.01],'booster':['gbtree','dart']}
+        gridsearch_xgb = GridSearchCV(estimator=xgb,param_grid=parameters,scoring='accuracy',cv=2)
         gridresult_xgb = gridsearch_xgb.fit(bowl_features,bowl_targets)
         xgb_best_score = gridresult_xgb.best_score_
         xgb_best_params = gridresult_xgb.best_params_
         best_score = [xgb_best_score,'xgb']
         best_params = xgb_best_params
             #RandomForestClassifier
-        parameters = [{'n_estimators':[75,100,125],'criterion':['gini','entropy'],'min_leaf_samples':[1,2,3]}]
-        gridsearch_rfc = GridSearchCV(estimator=rfc_model,param_grid=parameters,scoring='accuracy',cv=2)
+        parameters = {'n_estimators':[75,100,125],'criterion':['gini','entropy'],'min_leaf_samples':[1,2,3]}
+        gridsearch_rfc = GridSearchCV(estimator=rfc,param_grid=parameters,scoring='accuracy',cv=2)
         gridresult_rfc = gridsearch_rfc.fit(bowl_features,bowl_targets)
         rfc_best_score = gridresult_rfc.best_score_
         if best_score[0] < rfc_best_score:
             best_score = [rfc_best_score,'rfc']
             best_params = gridresult_rfc.best_params_
             #SupportVectorMachine
-        parameters = [{'C':[1,5,10],'kernel':['rbf','linear','sigmoid'],'gamma':['auto','scale']}]
-        gridsearch_svc = GridSearchCV(estimator=svc_model,param_grid=parameters,scoring='accuracy',cv=2)
+        parameters = {'C':[1,5,10],'kernel':['rbf','linear','sigmoid'],'gamma':['auto','scale']}
+        gridsearch_svc = GridSearchCV(estimator=svc,param_grid=parameters,scoring='accuracy',cv=2)
         gridresult_svc = gridsearch_svc.fit(bowl_features,bowl_targets)
         svc_best_score = gridresult_svc.best_score_
         if best_score[0] < svc_best_score:
@@ -223,17 +224,17 @@ def player_performance(param,player_name,opposition=None,venue=None):
         if best_score[1] == 'xgb':
             classifier = XGBclassifier(objective='multi:softmax',n_estimators=best_params['n_estimators'],learning_rate=best_params['learning_rate'],booster=best_params['booster'])
             classifier = classifier.fit(bowl_features,bowl_targets)
-            res = {'bowl_prediction':classifier.predict(predict_bowl_features[0])}
+            res = {'bowl_prediction':classifier.predict(predict_bowl_features)}
             #RandomForestClassifier
         elif best_score[1] == 'rfc':
             classifier = RandomForestClassifier(n_estimators=best_params['n_estiamtors'],criterion=best_params['criterion'],random_state=42,min_samples_leaf=best_params['min_leaf_samples'])
             classifier = classifier.fit(bowl_features,bowl_targets)
-            res = {'bowl_prediction':classifier.predict(predict_bowl_features[0])}
+            res = {'bowl_prediction':classifier.predict(predict_bowl_features)}
             #SupportVectorMachine
         elif best_score[1] == 'svc':
             classifier = SVC(C=best_params['C'],kernel=best_params['kernel'],gamma=best_params['gamma'])
             classifier = classifier.fit(bowl_features,bowl_targets)
-            res = {'bowl_prediction':classifier.predict(predict_bowl_features[0])}
+            res = {'bowl_prediction':classifier.predict(predict_bowl_features)}
 
         print('Bowling Prediction Ends!')
 
